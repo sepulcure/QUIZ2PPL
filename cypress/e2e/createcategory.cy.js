bec2bb5a43c76707baa6202cfd5a1b4fab862b67
sepulcure <1941720126@student.polinema.ac.id>
2022-11-30 09:55:22 +0700

    QUIZ2

--- cypress/e2e/createcategory.cy.js
+++ cypress/e2e/createcategory.cy.js
@@ -1,5 +1,27 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://cerita-online-kelompok6-ti2a.000webhostapp.com/admin/login_admin.php')
+
+    //Login
+    cy.get(':nth-child(1) > .form-control').type("rhd")
+    cy.get(':nth-child(2) > .form-control').type("raihan123")
+    cy.get('.btn').click();
+    cy.get('[href="tambah_kategori.php"]').click();
+
+    //Test 01
+    cy.get('#validationCustom01').type("Anime")
+    cy.get('.m-3 > .btn').click();
+
+    //Test 02
+    cy.get('.m-3 > .btn').click
+
+    //Test Case 03
+    cy.get('#validationCustom01').type("Adventure")
+    cy.get('.m-3 > .btn').click();
+
+    //Logout
+    cy.get('#navbarDropdown').click();
+    cy.get('.dropdown-item').click();
+
   })
 })
--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,5 +1,32 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Testing the application", function() {
+  it("should login with username and password", function() {
+
+      // visit the lambdageeks page
+      //Test S
+      cy.visit('https://cerita-online-kelompok6-ti2a.000webhostapp.com/admin/login_admin.php')
+
+      cy.get(':nth-child(1) > .form-control').type('rhd')
+
+      cy.get(':nth-child(2) > .form-control').type('raihan123')
+
+      cy.get('.btn').click()
+
+      cy.get('[href="tambah_kategori.php"]').click()
+      //Test Case 01
+    cy.get('#validationCustom01').type("Anime")
+    cy.get('.m-3 > .btn').click();
+
+    //Test Case 02
+    cy.get('.m-3 > .btn').click
+
+    //Test Case 03
+    cy.get('#validationCustom01').type("Adventure")
+    cy.get('.m-3 > .btn').click();
+
+    //Logout
+    cy.get('#navbarDropdown').click();
+    cy.get('.dropdown-item').click();
+
+      
+  });
+});
